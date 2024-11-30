4d71777b17a743075409261ab6e7a9baf4d436de
burnworks <6436833+burnworks@users.noreply.github.com>
2024-11-30 20:29:23 +0900

    Update JsonComma.pm

--- JsonComma/lib/MT/Plugin/JsonComma.pm
+++ JsonComma/lib/MT/Plugin/JsonComma.pm
@@ -1,18 +1,18 @@
 package MT::Plugin::JsonComma;
 use strict;
 use warnings;
-use base qw( MT::Plugin );
+use parent qw( MT::Plugin );
 
 sub _hdlr_jsoncomma {
     my ($ctx, $args) = @_;
     
     # 通常のループチェック
-    return q{} if ($ctx->var('__last__') || 0);
+    return q{} if $ctx->var('__last__');
     
     # カテゴリー内のループチェック（<mt:SubCategories top="1"> と <mt:TopLevelCategories> 向けの処理）
-    my $is_subcategory_last = ($ctx->stash('subCatIsLast') || 0);
+    my $is_subcategory_last = $ctx->stash('subCatIsLast');
     
-    # 各ループ内で最後のループなら空文字
+    # 各ループ内で最後のループなら空文字（この判定の仕方はちょっと良くない気がするがとりあえず）
     return q{} if ($is_subcategory_last && !defined($ctx->var('__last__')));
     
     # それ以外の場合はカンマを出力
